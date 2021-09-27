04d9175403808f5deecf9ce696e60d7ebd032cd4
xcrn <xcrnvieira@gmail.com>
2021-09-27 19:53:56 -0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import HomePage from '@/views/Home.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,31 +2,53 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Calc Raíz</ion-title>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">Calc Raíz</ion-title>
         </ion-toolbar>
       </ion-header>
     
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <form @submit.prevent="calculo">
+          <div>
+              <label>Calcular o quadrado do número: </label>
+          </div>
+          <input v-model.number="num1" type="number"/>
+           <ion-button type="submit">Calcular</ion-button>
+        </form>
+        <div>
+          <label>Resultado: {{ resultado }}</label>
+        </div>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script>
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Home',
+  data () {
+    return {
+      num1: '',
+      resultado: ''
+    }
+  },
+  methods: {
+    calculo () {
+      const { resultado, num1 } = this;
+      this.resultado = (num1 * num1);
+      console.log(resultado);
+      
+    }
+  },
   components: {
     IonContent,
     IonHeader,
@@ -40,12 +62,36 @@ export default defineComponent({
 <style scoped>
 #container {
   text-align: center;
-  
   position: absolute;
   left: 0;
   right: 0;
   top: 50%;
   transform: translateY(-50%);
+
+}
+
+form {
+  border: 1px solid #8c8c8c;
+  border-radius: 3px;
+  margin: 0 35% 1%;
+  padding: 25px;
+}
+
+input {
+  margin: 5px;
+  width: 80px;
+  height: 40px;
+  border: 0px;
+  border-bottom: 1px solid #8c8c8c;
+}
+
+button {
+  background-color: blue;
+  color: aliceblue;
+  width: 100px;
+  font-size: 14px;
+  padding: 5px 0;
+  border-radius: 3px;
 }
 
 #container strong {
